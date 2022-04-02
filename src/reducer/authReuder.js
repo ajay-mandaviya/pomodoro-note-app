db3385295086967533064343a6e73b657d7ef51f
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-04-03 00:42:15 +0530

    feat : add auth context

---
+++ src/reducer/authReuder.js
@@ -0,0 +1,24 @@
+import { SET_TOKEN, SET_USER, AUTH_LOADING } from "../constant/actionTypes";
+
+export const authReuder = (state, action) => {
+  switch (action.type) {
+    case AUTH_LOADING:
+      return {
+        ...state,
+        auth_loading: action.payload,
+      };
+    case SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload,
+      };
+
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
